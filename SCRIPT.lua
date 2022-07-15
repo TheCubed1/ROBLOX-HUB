ac9911ece6ad3f828cbe5e0d41f78ff7d45ffe2c
TheCubed1 <97459356+TheCubed1@users.noreply.github.com>
2022-07-15 13:15:02 -0500

    Update SCRIPT.lua

--- SCRIPT.lua
+++ SCRIPT.lua
@@ -1,5 +1,5 @@
 print "Cubed's Games Hub Loaded Succesfully"
   if game.PlaceId == 9508087919 then --Apeirophobia
     local http_request = http_request or request or (syn and syn.request) or (http and http.request)
-    loadstring(http_request({Url="https://raw.githubusercontent.com/TheCubed1/ROBLOX-HUB/GUIS/ApeirophobiaGUI.lua?token=GHSAT0AAAAAABWSWSQY5CFZM2QQQUZBHSNMYWRU65A", Method="GET"}).Body)()
+    loadstring(http_request({Url="https://raw.githubusercontent.com/TheCubed1/ROBLOX-HUB/GUIS/ApeirophobiaGUI.lua", Method="GET"}).Body)()
 end
