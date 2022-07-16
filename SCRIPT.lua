174c47a86d9524ab5aa02cacbffb5ffd97bec12e
TheCubed1 <97459356+TheCubed1@users.noreply.github.com>
2022-07-16 14:54:24 -0500

    Update SCRIPT.lua

--- SCRIPT.lua
+++ SCRIPT.lua
@@ -1,17 +1,11 @@
 print "Cubed's Games Hub Loaded Succesfully"
-print "
-
-"
+print "."
 print "Checking Current Game"
-print "
-
-"
+print "."
 wait(2.5)
   if game.PlaceId == 9508108517 then --Apeirophobia
     print"Current Game: Apeirophobia"
-    print "
-
-  "
+    print "."
     print"Game Supported"
     local http_request = http_request or request or (syn and syn.request) or (http and http.request)
     loadstring(http_request({Url="https://raw.githubusercontent.com/TheCubed1/ROBLOX-HUB/GUIS/ApeirophobiaGUI.lua", Method="GET"}).Body)()
